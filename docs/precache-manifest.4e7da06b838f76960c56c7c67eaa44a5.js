b0123a3a664cc49f76db696fec28ecb6e65218cb
Johnatan Dias <johnatan@take.net>
2019-12-13 01:24:43 -0300

    Added docs folder for GitHub pages

---
+++ docs/precache-manifest.4e7da06b838f76960c56c7c67eaa44a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ef0150de9d6938db58de70a17ffa01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33f6b63ae610240e9c75",
+    "url": "/static/css/main.e7e0d64d.chunk.css"
+  },
+  {
+    "revision": "66e120c2c6b31bd39d18",
+    "url": "/static/js/2.266f5f40.chunk.js"
+  },
+  {
+    "revision": "33f6b63ae610240e9c75",
+    "url": "/static/js/main.a81fdd05.chunk.js"
+  },
+  {
+    "revision": "0b55047e418454c3123f",
+    "url": "/static/js/runtime~main.091fb54a.js"
+  }
+]);
